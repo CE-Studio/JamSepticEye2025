d2b70df68c1260e60d6ef100b33cf3f861d3e0d0
EpsilonTheDerg <jamesclamey@gmail.com>
2025-10-01 23:13:34 +0100

    Level transition foundation

---
+++ scripts/transition_trigger.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+
+@export_file_path("*.tscn") var target_scene:String
+
+
+func _on_player_enter(_body) -> void:
+	if target_scene.strip_edges() != "":
+		get_tree().change_scene_to_file.call_deferred(target_scene)
