fb795ab94573352ddae7e9e134068a1bef5011f3
EpsilonTheDerg <jamesclamey@gmail.com>
2025-10-02 10:27:22 +0100

    Updated level transitions, UI object

--- scripts/transition_handler.gd
+++ scripts/transition_handler.gd
@@ -1,1 +1,15 @@
 extends Control
+
+
+var target_scene:String = ""
+
+@onready var anim:AnimationPlayer = $"AnimationPlayer"
+
+
+func begin_load_fadeout(target:String) -> void:
+	target_scene = target
+	anim.play("EndFadeIn")
+
+
+func _swap_scene() -> void:
+	get_tree().change_scene_to_file.call_deferred(target_scene)
