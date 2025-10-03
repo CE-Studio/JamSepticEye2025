e4d7bdab7463c24bc144a4fd39ff1cc441d181b2
clarence112 <clarencepacelli@gmail.com>
2025-10-03 16:34:32 -0400

    kabloie 2: kaboom harder

--- scripts/cube.gd
+++ scripts/cube.gd
@@ -4,7 +4,8 @@ class_name  Cube
 
 
 const EXPLOSION:PackedScene = preload("res://parts/components/explosionDecal.tscn")
-@onready var aud := $AudioStreamPlayer3D
+@onready var aud:AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var ray:RayCast3D = $RayCast3D
 
 
 func _on_body_entered(_body):
@@ -14,8 +15,12 @@ func _on_body_entered(_body):
 func pop():
 	if global_position.distance_to(Player.instance.global_position) <= 4:
 		Player.instance.die()
+	ray.global_rotation = Vector3.ZERO
+	ray.force_raycast_update()
 	var expl:Decal = EXPLOSION.instantiate()
-	
 	get_parent().add_child(expl)
-	expl.global_position = global_position
+	if ray.is_colliding():
+		if ray.get_collision_normal() == Vector3.UP:
+			var pos = ray.get_collision_point()
+			expl.global_position = pos
 	queue_free()
