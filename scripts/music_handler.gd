d559e4e9a57e8fac98056a0e3cea03496bb6bd02
clarence112 <clarencepacelli@gmail.com>
2025-10-05 19:23:06 -0400

    a

--- scripts/music_handler.gd
+++ scripts/music_handler.gd
@@ -1,8 +1,13 @@
 extends Node3D
 
 
-const ACTIVE_VOL_LIVE:float = 0.4
-const ACTIVE_VOL_DEAD:float = 0.15
+const ACTIVE_VOL_LIVE:float = 0.2
+const ACTIVE_VOL_DEAD:float = 0.1
+
+var f_live:float = ACTIVE_VOL_LIVE
+var f_dead:float = 0
+var r_live:float = ACTIVE_VOL_LIVE
+var r_dead:float = 0
 
 var i_live:int
 var i_dead:int
@@ -14,11 +19,18 @@ func _ready() -> void:
 	set_mus_live()
 
 
+func _process(delta: float) -> void:
+	r_live = move_toward(r_live, f_live, delta * 0.1)
+	r_dead = move_toward(r_dead, f_dead, delta * 0.1)
+	AudioServer.set_bus_volume_linear(i_live, r_live)
+	AudioServer.set_bus_volume_linear(i_dead, r_dead)
+
+
 func set_mus_live() -> void:
-	AudioServer.set_bus_volume_linear(i_live, ACTIVE_VOL_LIVE)
-	AudioServer.set_bus_volume_linear(i_dead, 0)
+	f_live = ACTIVE_VOL_LIVE
+	f_dead = 0
 
 
 func set_mus_dead() -> void:
-	AudioServer.set_bus_volume_linear(i_live, 0)
-	AudioServer.set_bus_volume_linear(i_dead, ACTIVE_VOL_DEAD)
+	f_live = 0
+	f_dead = ACTIVE_VOL_DEAD
